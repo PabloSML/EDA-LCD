954eaedc6a5abeb18b7da7279eea5949fe54791c
Awasaa <gap.pintos@gmail.com>
2019-05-05 15:38:33 -0300

    agregue un clear a la funcion de inicializacion

--- EDA LCD/HitachiHD44780Driver.cpp
+++ EDA LCD/HitachiHD44780Driver.cpp
@@ -45,6 +45,8 @@ FT_HANDLE* lcdInit(void)
 				printf("iniciando:se envio el comando %x al lcd\n", LCD_CLEAR_SCREEN | LCD_RS_LOW); //solo para imprimir valor,DEBBUG BORRAR DESPUES
 				lcdWriteIR(lcdHandle, LCD_ENTRY_MODE_SET);
 				printf("iniciando:se envio el comando %x al lcd\n", LCD_ENTRY_MODE_SET); //solo para imprimir valor,DEBBUG BORRAR DESPUES
+				Sleep(100);
+				lcdWriteIR(lcdHandle, LCD_CLEAR_SCREEN);
 			}
 		}
 		else
