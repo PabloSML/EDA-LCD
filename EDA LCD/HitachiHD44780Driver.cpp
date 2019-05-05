873afd38fb265c49eee35e35f36ea1f5e5afc734
Awasaa <gap.pintos@gmail.com>
2019-05-05 16:38:37 -0300

    seguro funca (=

--- EDA LCD/HitachiHD44780Driver.cpp
+++ EDA LCD/HitachiHD44780Driver.cpp
@@ -45,10 +45,6 @@ FT_HANDLE* lcdInit(void)
 				printf("iniciando:se envio el comando %x al lcd\n", LCD_CLEAR_SCREEN | LCD_RS_LOW); //solo para imprimir valor,DEBBUG BORRAR DESPUES
 				lcdWriteIR(lcdHandle, LCD_ENTRY_MODE_SET);
 				printf("iniciando:se envio el comando %x al lcd\n", LCD_ENTRY_MODE_SET); //solo para imprimir valor,DEBBUG BORRAR DESPUES
-				Sleep(100);
-				lcdWriteIR(lcdHandle, LCD_CLEAR_SCREEN);
-				Sleep(100);
-				lcdWriteIR(lcdHandle, 0x0e);
 			}
 		}
 		else
@@ -97,16 +93,16 @@ void lcdWriteNibble(FT_HANDLE * deviceHandler, BYTE value)
 	BYTE temp;
 	temp = value | LCD_EN_LOW;
 	FT_Write(deviceHandler, &temp, 1, &bytesWritten);
-	printf("Envio a lcd:se envio el comando %x al lcd\n", value); //solo para imprimir valor,DEBBUG BORRAR DESPUES
+	printf("Envio a lcd:se envio el comando %x al lcd\n", temp); //solo para imprimir valor,DEBBUG BORRAR DESPUES
 
 	temp = value | LCD_EN_HIGH;
 	FT_Write(deviceHandler, &temp, 1, &bytesWritten);
-	printf("Envio a lcd:se envio el comando %x al lcd\n", value); //solo para imprimir valor,DEBBUG BORRAR DESPUES
+	printf("Envio a lcd:se envio el comando %x al lcd\n", temp); //solo para imprimir valor,DEBBUG BORRAR DESPUES
 
 	Sleep(5);
 	temp = value & (LCD_NOT_EN_HIGH);
 	FT_Write(deviceHandler, &temp, 1, &bytesWritten);
-	printf("Envio a lcd:se envio el comando %x al lcd\n", value); //solo para imprimir valor,DEBBUG BORRAR DESPUES
+	printf("Envio a lcd:se envio el comando %x al lcd\n", temp); //solo para imprimir valor,DEBBUG BORRAR DESPUES
 }
 
 /*
